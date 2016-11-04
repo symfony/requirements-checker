71877c4ed83e1f33e4e84411436cf3cdbe1d5298
Javier Eguiluz <javier.eguiluz@gmail.com>
2016-11-04 08:34:55 +0100

    Renamed PhpIni*() to PhpConfig*()

--- src/Requirement.php
+++ src/Requirement.php
@@ -14,7 +14,8 @@ namespace Symfony\Requirements;
 /**
  * Represents a single PHP requirement, e.g. an installed extension.
  * It can be a mandatory requirement or an optional recommendation.
- * There is a special subclass, named PhpIniRequirement, to check a php.ini configuration.
+ * There is a special subclass, named PhpConfigRequirement, to check a PHP
+ * configuration option.
  *
  * @author Tobias Schultze <http://tobion.de>
  */
