32b9f96bbf8413f66e5db7a7b461a0b261faf68d
Fabien Potencier <fabien.potencier@gmail.com>
2018-04-17 14:08:26 +0200

    fixed missing backslash

--- src/PhpConfigRequirement.php
+++ src/PhpConfigRequirement.php
@@ -38,7 +38,7 @@ class PhpConfigRequirement extends Requirement
 
         if (is_callable($evaluation)) {
             if (null === $testMessage || null === $helpHtml) {
-                throw new InvalidArgumentException('You must provide the parameters testMessage and helpHtml for a callback evaluation.');
+                throw new \InvalidArgumentException('You must provide the parameters testMessage and helpHtml for a callback evaluation.');
             }
 
             $fulfilled = call_user_func($evaluation, $cfgValue);
