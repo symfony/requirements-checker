d1505e76b667eafc23e6788fa72e1eea91e36a65
Fabien Potencier <fabien@potencier.org>
2020-09-15 09:13:34 +0200

    Fix dir detection without vendor

--- bin/requirements-checker.php
+++ bin/requirements-checker.php
@@ -40,7 +40,7 @@ $symfonyRequirements = new SymfonyRequirements();
 $requirements = $symfonyRequirements->getRequirements();
 
 // specific directory to check?
-$dir = isset($args[1]) ? $args[1] : dirname(dirname(realpath($autoloader)));
+$dir = isset($args[1]) ? $args[1] : (file_exists(getcwd().'/composer.json') ? getcwd().'/composer.json' : null);
 if (null !== $dir) {
     $projectRequirements = new ProjectRequirements($dir);
     $requirements = array_merge($requirements, $projectRequirements->getRequirements());
